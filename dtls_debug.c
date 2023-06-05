298d38b15886fe1560d61bddee19b9f996af53ed
flaz83 <flavio.ferrandi@gmail.com>
2023-06-05 14:31:30 +0200

    chore: remove session socket dependency

--- dtls_debug.c
+++ dtls_debug.c
@@ -24,20 +24,9 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-#ifdef HAVE_ARPA_INET_H
-#include <arpa/inet.h>
-#endif
-
 #ifdef HAVE_TIME_H
 #include <time.h>
 #endif
-
-#ifdef WITH_ZEPHYR
-#ifdef HAVE_NET_SOCKET_H
-#include <net/socket.h>
-#endif /* HAVE_NET_SOCKET_H */
-typedef int in_port_t;
-#endif /* WITH_ZEPHYR */
 
 #include "global.h"
 #include "dtls_debug.h"
@@ -47,19 +36,7 @@ typedef int in_port_t;
 #define min2(a,b) ((a) < (b) ? (a) : (b))
 #endif
 
-#ifdef WITH_ZEPHYR
-LOG_MODULE_REGISTER(TINYDTLS, CONFIG_TINYDTLS_LOG_LEVEL);
-#endif /* WITH_ZEPHYR */
-
 static int maxlog = DTLS_LOG_WARN;      /* default maximum log level */
-
-const char *dtls_package_name(void) {
-  return PACKAGE_NAME;
-}
-
-const char *dtls_package_version(void) {
-  return PACKAGE_VERSION;
-}
 
 log_t
 dtls_get_log_level(void) {
@@ -117,127 +94,6 @@ dtls_strnlen(const char *s, size_t maxlen) {
   while(*s++ && n < maxlen)
     ++n;
   return n;
-}
-
-/**
- * Write service-address as text to buffer.
- *
- * The text is \000 terminated.
- *
- * \param addr   The session including the sockaddr.
- * \param buf    The buffer to write the service address into.
- * \param len    The actual length of \p buf.
- * \return Less than zero on error, the number of bytes written otherwise.
- */
-static size_t
-dsrv_print_addr(const session_t *addr, char *buf, size_t len) {
-#ifdef HAVE_INET_NTOP
-  const void *addrptr = NULL;
-  in_port_t port;
-  char *p = buf;
-  size_t append;
-  int err;
-
-  /* max. '[' ipv6 ']:' port '\0' */
-  assert(len >= 1 + INET6_ADDRSTRLEN + 2 + 5 + 1);
-
-  switch (addr->addr.sa.sa_family) {
-  case AF_INET:
-    addrptr = &addr->addr.sin.sin_addr;
-    port = ntohs(addr->addr.sin.sin_port);
-    break;
-  case AF_INET6:
-    *p++ = '[';
-    --len;
-    addrptr = &addr->addr.sin6.sin6_addr;
-    port = ntohs(addr->addr.sin6.sin6_port);
-    break;
-  default:
-    /* include terminating \000 */
-    append = strlen("(unknown address type)");
-    memcpy(p, "(unknown address type)", append + 1);
-    return append;
-  }
-
-  if (inet_ntop(addr->addr.sa.sa_family, addrptr, p, len) == 0) {
-    perror("dsrv_print_addr");
-    return 0;
-  }
-
-  /* append inet_ntop to p */
-  append = dtls_strnlen(p, len);
-  p += append;
-  len -= append;
-
-  if (addr->addr.sa.sa_family == AF_INET6) {
-    *p++ = ']';
-    --len;
-  }
-
-  /* append port and \000 termination */
-  err = snprintf(p, len, ":%d", port);
-  if (err < 0) {
-    return 0;
-  }
-  p += err;
-  len -= err;
-
-  return p - buf;
-#else /* ! HAVE_INET_NTOP */
-
-#ifdef WITH_CONTIKI
-  char *p = buf;
-#if NETSTACK_CONF_WITH_IPV6
-  uint8_t i;
-  const char hex[] = "0123456789ABCDEF";
-
-  if (len < 41)
-    return 0;
-
-  *p++ = '[';
-
-  for (i=0; i < 16; i += 2) {
-    if (i) {
-      *p++ = ':';
-    }
-    *p++ = hex[(addr->addr.u8[i] & 0xf0) >> 4];
-    *p++ = hex[(addr->addr.u8[i] & 0x0f)];
-    *p++ = hex[(addr->addr.u8[i+1] & 0xf0) >> 4];
-    *p++ = hex[(addr->addr.u8[i+1] & 0x0f)];
-  }
-  *p++ = ']';
-#else /* NETSTACK_CONF_IPV6 */
-  if (len < 21)
-    return 0;
-
-  p += sprintf(p, "%u.%u.%u.%u",
-               addr->addr.u8[0], addr->addr.u8[1],
-               addr->addr.u8[2], addr->addr.u8[3]);
-#endif /* NETSTACK_CONF_IPV6 */
-  if (buf + len - p < 6)
-    return 0;
-
-  p += sprintf(p, ":%d", uip_htons(addr->port));
-
-  return p - buf;
-
-#endif /* WITH_CONTIKI */
-
-#if defined(RIOT_VERSION) || defined(IS_WINDOWS) || defined(WITH_LWIP) || defined(IS_MBEDOS)
-  /* FIXME: Switch to RIOT own DEBUG lines */
-  /* TODO: Check if inet_ntop can be used on Windows */
-  (void) addr;
-  (void) buf;
-  (void) len;
-#endif /* RIOT_VERSION || IS_WINDOWS */
-
-#ifdef WITH_POSIX
-  /* TODO: output addresses manually */
-#warning "inet_ntop() not available, network addresses will not be included in debug output"
-#endif /* WITH_POSIX */
-
-  return 0;
-#endif /* ! HAVE_INET_NTOP */
 }
 
 #endif /* NDEBUG */
@@ -362,32 +218,7 @@ void dump(unsigned char *buf, size_t len) {
 void dtls_dsrv_log_addr(log_t level, const char *name, const session_t *addr)
 {
   char addrbuf[73];
-  int len;
-
-  len = dsrv_print_addr(addr, addrbuf, sizeof(addrbuf));
-  if (!len)
-    return;
-#ifdef WITH_ZEPHYR
-  switch(level) {
-    case DTLS_LOG_EMERG:
-    case DTLS_LOG_ALERT:
-    case DTLS_LOG_CRIT:
-      Z_LOG(LOG_LEVEL_ERR, "%s: %s\n", name, addrbuf);
-      break;
-    case DTLS_LOG_WARN:
-      Z_LOG(LOG_LEVEL_WRN, "%s: %s\n", name, addrbuf);
-      break;
-    case DTLS_LOG_NOTICE:
-    case DTLS_LOG_INFO:
-      Z_LOG(LOG_LEVEL_INF, "%s: %s\n", name, addrbuf);
-      break;
-    case DTLS_LOG_DEBUG:
-      Z_LOG(LOG_LEVEL_DBG, "%s: %s\n", name, addrbuf);
-      break;
-  }
-#else /* WITH_ZEPHYR */
   dsrv_log(level, "%s: %s\n", name, addrbuf);
-#endif /* WITH_ZEPHYR */
 }
 
 #ifndef WITH_CONTIKI
--- dtls_debug.h
+++ dtls_debug.h
@@ -24,48 +24,10 @@
 #include "global.h"
 #include "session.h"
 
-#ifdef WITH_ZEPHYR
-#include <logging/log.h>
-#endif /* WITH_ZEPHYR */
-
-#ifdef WITH_CONTIKI
-# ifndef DEBUG
-#  define DEBUG DEBUG_PRINT
-# endif /* DEBUG */
-#include "net/ip/uip-debug.h"
-
-#ifdef CONTIKI_TARGET_MBXXX
-extern char __Stack_Init, _estack;
-
-static inline void check_stack(void) {
-  const char *p = &__Stack_Init;
-  while (p < &_estack && *p == 0x38) {
-    p++;
-  }
-
-  PRINTF("Stack: %d bytes used (%d free)\n", &_estack - p, p - &__Stack_Init);
-}
-#else /* CONTIKI_TARGET_MBXXX */
-static inline void check_stack(void) {
-}
-#endif /* CONTIKI_TARGET_MBXXX */
-#else /* WITH_CONTKI */
-#define PRINTF(...)
-
-static inline void check_stack(void) {
-}
-#endif
-
 /** Pre-defined log levels akin to what is used in \b syslog. */
 typedef enum { DTLS_LOG_EMERG=0, DTLS_LOG_ALERT, DTLS_LOG_CRIT, DTLS_LOG_WARN, 
        DTLS_LOG_NOTICE, DTLS_LOG_INFO, DTLS_LOG_DEBUG
 } log_t;
-
-/** Returns a zero-terminated string with the name of this library. */
-const char *dtls_package_name(void);
-
-/** Returns a zero-terminated string with the library version. */
-const char *dtls_package_version(void);
 
 /** Returns the current log level. */
 log_t dtls_get_log_level(void);
@@ -91,42 +53,6 @@ typedef void (*dtls_log_handler_t) (log_t level, const char *message);
  */
 void dtls_set_log_handler(dtls_log_handler_t app_handler);
 
-/** 
- * Writes the given text to \c stdout. The text is output only when \p
- * level is below or equal to the log level that set by
- * set_log_level(). */
-#ifdef HAVE_VPRINTF
-#if (defined(__GNUC__) && !defined(__MINGW32__))
-void dsrv_log(log_t level, const char *format, ...) __attribute__ ((format(printf, 2, 3)));
-#else /* !__GNUC__ && !__MINGW32__ */
-void dsrv_log(log_t level, const char *format, ...);
-#endif /* !__GNUC__ && !__MINGW32__ */
-#else
-#define dsrv_log(level, format, ...) PRINTF(format, ##__VA_ARGS__)
-#endif
-
-/** dumps packets in usual hexdump format */
-void hexdump(const unsigned char *packet, int length);
-
-/** dump as narrow string of hex digits */
-void dump(unsigned char *buf, size_t len);
-
-void dtls_dsrv_hexdump_log(log_t level, const char *name, const unsigned char *buf, size_t length, int extend);
-
-void dtls_dsrv_log_addr(log_t level, const char *name, const session_t *addr);
-
-/* A set of convenience macros for common log levels. */
-#ifdef WITH_ZEPHYR
-#define dtls_emerg(...) LOG_ERR(__VA_ARGS__)
-#define dtls_alert(...) LOG_ERR(__VA_ARGS__)
-#define dtls_crit(...) LOG_ERR(__VA_ARGS__)
-#define dtls_warn(...) LOG_WRN(__VA_ARGS__)
-#define dtls_notice(...) LOG_INF(__VA_ARGS__)
-#define dtls_info(...) LOG_INF(__VA_ARGS__)
-#define dtls_debug(...) LOG_DBG(__VA_ARGS__)
-#define dtls_debug_hexdump(name, buf, length) { LOG_DBG("%s (%zu bytes):", name, length); LOG_HEXDUMP_DBG(buf, length, name); }
-#define dtls_debug_dump(name, buf, length) { LOG_DBG("%s (%zu bytes):", name, length); LOG_HEXDUMP_DBG(buf, length, name); }
-#else /* WITH_ZEPHYR */
 #define dtls_emerg(...) dsrv_log(DTLS_LOG_EMERG, __VA_ARGS__)
 #define dtls_alert(...) dsrv_log(DTLS_LOG_ALERT, __VA_ARGS__)
 #define dtls_crit(...) dsrv_log(DTLS_LOG_CRIT, __VA_ARGS__)
@@ -136,6 +62,5 @@ void dtls_dsrv_log_addr(log_t level, const char *name, const session_t *addr);
 #define dtls_debug(...) dsrv_log(DTLS_LOG_DEBUG, __VA_ARGS__)
 #define dtls_debug_hexdump(name, buf, length) dtls_dsrv_hexdump_log(DTLS_LOG_DEBUG, name, buf, length, 1)
 #define dtls_debug_dump(name, buf, length) dtls_dsrv_hexdump_log(DTLS_LOG_DEBUG, name, buf, length, 0)
-#endif /* WITH_ZEPHYR */
 
 #endif /* _DTLS_DEBUG_H_ */
